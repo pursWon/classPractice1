2a094d3b5030aa60ee6fb0a82533759a4a19b9e0
Wongi <hpurs95@gmail.com>
2022-08-03 17:22:25 +0900

    Initial Commit

---
+++ classPractice1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  classPractice1
+//
+//  Created by MacBook Air on 2022/08/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
